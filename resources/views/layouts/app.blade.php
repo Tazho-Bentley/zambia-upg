cc34d9934d0d61844631a2b729aee5057c45121c
kuma93 <kuma.musenge@gmail.com>
2016-05-24 09:59:10 +0200

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,7 @@
                                         <li><a href="{{ url('/register') }}"><i class="fa fa-user"></i>Register</a></li>
                                     @else
                                         <li class="dropdown">
-                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="true">
                                                 {{ Auth::user()->name }} <span class="caret"></span>
                                             </a>
 
